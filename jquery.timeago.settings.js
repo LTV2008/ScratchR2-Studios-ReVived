063b00805e9cf98bc057e899356aa11f5460df16
LTV2008 <112987585+LTV2008@users.noreply.github.com>
2023-03-29 20:10:11 -0400

    Assets made

---
+++ jquery.timeago.settings.js
@@ -0,0 +1,54 @@
+var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
+if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
+{
+  let window = _____WB$wombat$assign$function_____("window");
+  let self = _____WB$wombat$assign$function_____("self");
+  let document = _____WB$wombat$assign$function_____("document");
+  let location = _____WB$wombat$assign$function_____("location");
+  let top = _____WB$wombat$assign$function_____("top");
+  let parent = _____WB$wombat$assign$function_____("parent");
+  let frames = _____WB$wombat$assign$function_____("frames");
+  let opener = _____WB$wombat$assign$function_____("opener");
+
+$.timeago.settings.strings = {
+  prefixAgo: gettext(" "),
+  prefixFromNow: gettext("  "),
+  suffixAgo: gettext("ago"),
+  suffixFromNow: gettext("from now"),
+  inPast: gettext("any moment now"),
+  seconds: gettext("less than a minute"),
+  minute: gettext("about a minute"),
+  minutes: gettext("%d minutes"),
+  hour: gettext("about an hour"),
+  hours: gettext("about %d hours"),
+  day: gettext("a day"),
+  days: gettext("%d days"),
+  month: gettext("about a month"),
+  months: gettext("%d months"),
+  year: gettext("about a year"),
+  years: gettext("%d years"),
+  wordSeparator: " ",
+  numbers: []
+};
+
+}
+/*
+     FILE ARCHIVED ON 09:47:47 Oct 30, 2019 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 23:53:09 Mar 29, 2023.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  captures_list: 193.266
+  exclusion.robots: 0.067
+  exclusion.robots.policy: 0.058
+  cdx.remote: 0.055
+  esindex: 0.008
+  LoadShardBlock: 109.688 (3)
+  PetaboxLoader3.datanode: 422.561 (4)
+  PetaboxLoader3.resolve: 104.957 (2)
+  load_resource: 1135.873
+*/
